adf602f6b5bc3c848a6ce131b11558d26712bf00
Deldar777 <54842815+Deldar777@users.noreply.github.com>
2020-09-18 17:49:57 +0200

    MVC tutorial-18-09

---
+++ SaleSystem/Shekho/Controllers/MoviesController.cs
@@ -0,0 +1,60 @@
+﻿using Shekho.Models;
+using Shekho.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.MappingViews;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shekho.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+
+
+        
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer{Name = "Jack",ID =1},
+                new Customer{Name = "Roze",ID =2}
+            };
+
+            var ViewModel = new RandomViewMovieModel
+            {
+                Movie = movie,
+                customers = customers
+            };
+
+            return View(ViewModel);
+            
+        }
+
+        [Route("eerst/tweede/{year}/{month:range(1,12)}")]
+        public ActionResult display(int year,int month)
+        {
+            return Content(year + "/" + month);
+        }
+        
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id=" + id);
+        }
+
+        /*public ActionResult Index(int? pageIndex,string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Shekho";
+
+            return Content(String.Format("pageIndex= {0} &sortBy={1}",pageIndex,sortBy));
+
+        }*/
+    }
+}
---
+++ SaleSystem/Shekho/ViewModels/RandomViewMovieModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Shekho.Models;
+
+namespace Shekho.ViewModels
+{
+    public class RandomViewMovieModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> customers { get; set; }
+    }
+}
